4abdff929d02b580e05e9f35ebb23b98a38e75ab
amadeus <amadeusdias@hotmail.com>
2023-05-17 15:12:23 -0300

    primeiro req

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -1,5 +1,6 @@
 from functools import lru_cache
 from typing import List, Dict
+import csv
 
 
 @lru_cache
@@ -16,7 +17,11 @@ def read(path: str) -> List[Dict]:
     list
         List of rows as dicts
     """
-    raise NotImplementedError
+    with open(path) as file:
+        content = csv.DictReader(file)
+        jobs = [job for job in content]
+
+    return jobs
 
 
 def get_unique_job_types(path: str) -> List[str]:
