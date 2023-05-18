3aba14a5bcc57b67ebe2a7c388ff0ac57ff0ff14
amadeus <amadeusdias@hotmail.com>
2023-05-18 10:32:23 -0300

    requisito 3, com if

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -1,4 +1,5 @@
 from typing import List, Dict
+from src.insights.jobs import read
 
 
 def get_unique_industries(path: str) -> List[str]:
@@ -16,7 +17,13 @@ def get_unique_industries(path: str) -> List[str]:
     list
         List of unique industries
     """
-    raise NotImplementedError
+    jobs = read(path)
+    # set_industry_types = set()
+    # for industry in jobs:
+    #     set_industry_types.add(industry["industry"])
+    return {
+        industry["industry"] for industry in jobs if industry["industry"] != ""
+    }
 
 
 def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
