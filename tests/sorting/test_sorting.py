0fbf26e39595acfbc351da1b9de319df5bbe6259
amadeus <amadeusdias@hotmail.com>
2023-05-21 21:57:43 -0300

    requisito 11

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,20 @@
-# from src.pre_built.sorting import sort_by
+from src.pre_built.sorting import sort_by
 
 
 def test_sort_by_criteria():
-    pass
+    initial_list = [
+        {
+            "title": "Web developer",
+            "min_salary": "invalid",
+            " max_salary": "invalid",
+        },
+        {"title": "Front end developer", "min_salary": "1000"},
+        {"title": "Back end developer", "max_salary": "3000"},
+        {
+            "title": "Full stack end developer",
+            "min_salary": "4000",
+            " max_salary": "8000",
+        },
+    ]
+
+    assert sort_by(initial_list, "max_salary") == initial_list
